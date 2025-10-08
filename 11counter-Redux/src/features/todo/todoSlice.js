1d975d75486dccd320545871c4f4b578b544f5a2
saranshsahu123 <saranshsahu532@gmail.com>
2025-10-08 14:19:02 +0530

    add new folder

---
+++ 11counter-Redux/src/features/todo/todoSlice.js
@@ -0,0 +1,36 @@
+import { createSlice , nanoid } from "@reduxjs/toolkit";
+
+const intialState = {
+    todos : [{
+        id : 1,
+        text : "Hello world "
+        }]
+}
+
+
+
+
+export const todoSlice = createSlice({
+    name : 'todo',
+    intialState ,
+    reducers : {
+        addTodo : (state , action)=>{ 
+            const todo = {
+        id : nanoid()  ,
+        text : action.payload
+        }
+
+        state.todos.push(todo)
+        },
+        removeTodo :(state , action)=> {
+
+            state.todos = state.todos.filter(() => todo.id != action.payload )
+
+        } , 
+   
+    }
+})
+
+export const {addTodo , removeTodo } = todoSlice.actions 
+
+export default todoSlice.reducer
